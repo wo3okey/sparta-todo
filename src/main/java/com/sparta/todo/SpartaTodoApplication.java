b27a3aca5255c9de7b411e4e5efd8b172de2c6b7
wookey(안형욱) <wookey.ahn@bunjang.co.kr>
2024-08-08 17:39:18 +0900

    chore(init): 프로젝트 생성

---
+++ src/main/java/com/sparta/todo/SpartaTodoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/todo/SpartaTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
