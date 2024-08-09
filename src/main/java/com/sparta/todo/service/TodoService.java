a124a465d613c44f7cc5b707835c475460ce6432
wookey <anbriaao@naver.com>
2024-08-09 15:36:06 +0900

    feat(level3): 일정 다건 조회

--- src/main/java/com/sparta/todo/service/TodoService.java
+++ src/main/java/com/sparta/todo/service/TodoService.java
@@ -7,6 +7,9 @@ import com.sparta.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class TodoService {
@@ -20,5 +23,12 @@ public class TodoService {
     public TodoResponse getTodo(long todoId) {
         Todo entity = todoRepository.findById(todoId).orElseThrow();
         return TodoResponse.of(entity);
+    }
+
+    public List<TodoResponse> getTodos(String date, String managerName) {
+        List<Todo> entities = todoRepository.search(date, managerName);
+        return entities.stream()
+                .map(TodoResponse::of)
+                .collect(Collectors.toList());
     }
 }
