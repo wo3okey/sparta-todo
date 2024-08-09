7ec93b1b73a8ff4da4193193ef51ddd1792153a9
wookey <anbriaao@naver.com>
2024-08-09 15:48:13 +0900

    feat(level4): 일정 수정

--- src/main/java/com/sparta/todo/controller/TodoController.java
+++ src/main/java/com/sparta/todo/controller/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,15 @@ public class TodoController {
             @RequestParam(required = false) String managerName
     ) {
         List<TodoResponse> response = todoService.getTodos(date, managerName);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/api/v1/todos/{todoId}")
+    public ResponseEntity<TodoResponse> updateTodo(
+            @PathVariable long todoId,
+            @RequestBody TodoRequest request
+    ) {
+        TodoResponse response = todoService.updateTodo(todoId, request);
         return ResponseEntity.ok(response);
     }
 }
