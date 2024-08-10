277beb5d6f2612a6c157f240fd986b81c1ef1ff9
wookey <anbriaao@naver.com>
2024-08-10 14:10:52 +0900

    feat(level9): validation

--- src/main/java/com/sparta/todo/dto/TodoRequest.java
+++ src/main/java/com/sparta/todo/dto/TodoRequest.java
@@ -1,12 +1,17 @@
 package com.sparta.todo.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
 public class TodoRequest {
+    @NotBlank(message = "할일 제목은 필수 값입니다.")
+    @Size(max = 200, message = "할일 제목은 최대 200자 이내여야 합니다.")
     private final String todo;
     private final Long managerId;
+    @NotBlank(message = "비밀번호는 필수 값입니다.")
     private final String password;
 }
